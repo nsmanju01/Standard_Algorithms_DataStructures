3bbc610eeed02a9b811873def094cdbe96cbbee4
nsmanju01 <nsmanju01@gmail.com>
2018-01-19 22:18:22 -0600

    Added new comment

--- DynamicProgamming/Unique-path-with-obstacles.cpp
+++ DynamicProgamming/Unique-path-with-obstacles.cpp
@@ -19,7 +19,7 @@ public:
         for(int i = 1;i < m; i++)
             for(int j=1;j<n;j++)
                 obstacleGrid[i][j] = obstacleGrid[i][j] == 1 ? 0 : obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-        
+        //return the final value,present at the corner end of the given matrix
         return obstacleGrid[m-1][n-1];
 
     }
